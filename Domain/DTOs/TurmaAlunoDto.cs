067861afae55ed23e250ac7f27e5235f13574df4
Leonardo Dantas <llndantasf@gmail.com>
2024-05-23 20:50:40 -0300

    Criação dos Dtos
    -Criação dos dtos para manipulação de dados.

---
+++ Domain/DTOs/TurmaAlunoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.DTOs
+{
+    public class TurmaAlunoDto
+    {
+        public int Id { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public bool Ativo { get; set; }
+        public int TurmaId { get; set; }
+        public int AlunoId { get; set; }
+    }
+}
---
+++ Domain/DTOs/TurmaDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.DTOs
+{
+    public class TurmaDto
+    {
+        public int Id { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public bool Ativo { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
