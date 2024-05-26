80b1137b7d5cb0ad23039c40b6a2d5e448faa71c
Leonardo Dantas <llndantasf@gmail.com>
2024-05-26 14:12:53 -0300

    Ajuste na interface vinculação turma aluno
    -Ajuste na inteface da lista de turma aluno e form.
    -Como tambem a chamada do service para trazer alunos vinculados a turma como tambem da interface de vinculação
    -Implementação de mensagens de alerta toast

--- UI/TrackClassApp/src/app/features/turma-aluno/turma-aluno-form/turma-aluno-form.component.ts
+++ UI/TrackClassApp/src/app/features/turma-aluno/turma-aluno-form/turma-aluno-form.component.ts
@@ -2,55 +2,126 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TurmaAlunoService } from '../../../core/services/turma-aluno.service';
-import { TurmaAluno } from '../../../core/models/turma-aluno.model';
+import { AlunoService } from '../../../core/services/aluno.service';
+import { TurmaService } from '../../../core/services/turma.service';
+import { MessageService } from 'primeng/api';
+
+export interface Aluno {
+  id: number;
+  nome: string;
+}
+
+export interface Turma {
+  id: number;
+  nome: string;
+}
 
 @Component({
   selector: 'app-turma-aluno-form',
   templateUrl: './turma-aluno-form.component.html',
-  styleUrls: ['./turma-aluno-form.component.css']
+  styleUrls: ['./turma-aluno-form.component.css'],
+  providers: [MessageService]
 })
 export class TurmaAlunoFormComponent implements OnInit {
   turmaAlunoForm: FormGroup;
-  turmaAlunoId: number | null = null;
+  AlunoId: number | null = null;
+  isEdit: boolean = false;
+  isDisabled: boolean = false;
+  viewFlag: string | null = null;
+  listaAlunos: Aluno[] = [];
+  listaTurmas: Turma[] = [];
+  displayDialog: boolean = false;
 
   constructor(
     private fb: FormBuilder,
     private turmaAlunoService: TurmaAlunoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private alunoService: AlunoService,
+    private turmaService: TurmaService,
+    private messageService: MessageService
   ) {
     this.turmaAlunoForm = this.fb.group({
-      nome: ['', Validators.required]
+      ativo: [true],
+      turma: ['', Validators.required],
+      aluno: ['']
     });
   }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      const id = params.get('id');
-      this.turmaAlunoId = id ? +id : null;
-      if (this.turmaAlunoId) {
-        this.turmaAlunoService.getTurmaAluno(this.turmaAlunoId).subscribe(data => {
-          this.turmaAlunoForm.patchValue(data);
-        });
-      }
+    this.AlunoId = this.route.snapshot.params['id'] ? +this.route.snapshot.params['id'] : null;
+    this.viewFlag = this.router.url;
+    //this.isEdit = this.AlunoId != null;
+
+    this.getListaAlunos();
+    this.getListaTurmas();
+    this.displayDialog = true;
+
+    /*if(this.isDisabled = this.viewFlag.includes('turma/view')){
+      this.turmaAlunoForm.get('ativo')?.disable();
+      this.turmaAlunoForm.get('turma')?.disable();
+      this.turmaAlunoForm.get('aluno')?.disable();
+    }
+    if (this.AlunoId) {
+      this.turmaAlunoService.getTurmaAluno(this.AlunoId).subscribe((Response) =>
+        {
+          this.turmaAlunoForm.patchValue({
+            ativo: Response.ativo,
+            turma: Response.turmaId,
+            aluno: Response.alunoId,
+          });
+        }
+      );
+    }
+    */
+  }
+
+  getListaAlunos(): void {
+    this.alunoService.getAlunos().subscribe(alunos => {
+      this.listaAlunos = alunos.map(aluno => ({ id: aluno.id, nome: `${aluno.nome} ${aluno.sobrenome}`  }));
     });
   }
 
-  onSubmit() {
+  getListaTurmas(): void {
+    this.turmaService.getTurmas().subscribe(turmas => {
+      this.listaTurmas = turmas.map(turma => ({ id: turma.id, nome: turma.nome }));
+    });
+  }
+
+  onSubmit(): void {
     if (this.turmaAlunoForm.valid) {
-      if (this.turmaAlunoId) {
-        this.turmaAlunoService.updateTurmaAluno({ id: this.turmaAlunoId, ...this.turmaAlunoForm.value }).subscribe(() => {
-          this.router.navigate(['/turma-alunos']);
-        });
-      } else {
-        this.turmaAlunoService.addTurmaAluno(this.turmaAlunoForm.value).subscribe(() => {
-          this.router.navigate(['/turma-alunos']);
-        });
-      }
+      const turmaData = this.turmaAlunoForm.value;
+      turmaData.alunoId = this.AlunoId;
+      turmaData.turmaId = turmaData.turma.id;
+      turmaData.dataCriacao = new Date().toISOString();
+      turmaData.ativo = true;
+
+      this.turmaAlunoService.existeTurmaAluno(turmaData.alunoId, turmaData.turmaId).subscribe(
+        (exists) => {
+          if (exists) {
+            this.messageService.add({ severity: 'error', summary: 'Erro', detail: 'O aluno já está adicionado a essa turma' });
+          } else {
+            this.turmaAlunoService.addTurmaAluno(turmaData).subscribe(
+              (response) => {
+                this.messageService.add({ severity: 'success', summary: 'Sucesso', detail: 'Aluno adicionada a Turma com sucesso' });
+                setTimeout(() => {
+                  this.router.navigate(['/turma-aluno']);
+                }, 2000);
+              },
+              (error) => {
+                this.messageService.add({ severity: 'error', summary: 'Erro', detail: 'Erro ao adicionar Turma Aluno' });
+              }
+            );
+          }
+        },
+        (error) => {
+          this.messageService.add({ severity: 'error', summary: 'Erro', detail: 'Erro ao verificar Turma Aluno' });
+        }
+      );
     }
   }
 
-  cancel() {
-    this.router.navigate(['/turma-alunos']);
+  onCancel(): void {
+    this.router.navigate(['/turma-aluno']);
   }
 }
--- UI/TrackClassApp/src/app/features/turma-aluno/turma-aluno-list/turma-aluno-list.component.ts
+++ UI/TrackClassApp/src/app/features/turma-aluno/turma-aluno-list/turma-aluno-list.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { TurmaAlunoService } from '../../../core/services/turma-aluno.service';
 import { TurmaAluno } from '../../../core/models/turma-aluno.model';
+import { SearchByAlunoId } from '../../../core/models/searchByAlunoId.model';
+import { Aluno } from '../../../core/models/aluno.model';
+import { Turma } from '../../../core/models/turma.model';
+import { Router } from '@angular/router'
+import { AlunoService } from '../../../core/services/aluno.service';
+import { TurmaService } from '../../../core/services/turma.service';
+import { TurmaAlunoService } from '../../../core/services/turma-aluno.service';
+
 
 @Component({
   selector: 'app-turma-aluno-list',
@@ -8,23 +15,77 @@ import { TurmaAluno } from '../../../core/models/turma-aluno.model';
   styleUrls: ['./turma-aluno-list.component.css']
 })
 export class TurmaAlunoListComponent implements OnInit {
-  turmaAlunos: TurmaAluno[] = [];
+  alunos: Aluno[] = [];
+  filteredAlunos: Aluno[] = [];
+  filter: string = '';
+  displayedColumns: string[] = ['nome', 'sobrenome', 'email', 'dataNascimento', 'ativo', 'actions'];
+  selectedAluno: Aluno | null = null;
+  turmas: Turma[] = [];
+  displayDialog: boolean = false;
+  displayDialogAdd: boolean = false;
+  alunoId: number | null = null;
+  turmaAlunos: SearchByAlunoId[] = [];
+  filteredTurmaAlunos: SearchByAlunoId[] = [];
+  errorMessage: string = '';
 
-  constructor(private turmaAlunoService: TurmaAlunoService) { }
+  constructor(private alunoService: AlunoService, private turmaService: TurmaService, private turmaAlunoService: TurmaAlunoService, private router: Router) {}
 
   ngOnInit(): void {
-    this.loadTurmaAlunos();
-  }
-
-  loadTurmaAlunos() {
-    this.turmaAlunoService.getTurmaAlunos().subscribe(data => {
-      this.turmaAlunos = data;
+    this.alunoService.getAlunos().subscribe(alunos => {
+      this.alunos = alunos;
+      this.filteredAlunos = alunos;
     });
   }
 
-  deleteTurmaAluno(id: number) {
+  filterAlunos(): void {
+    this.filteredAlunos = this.alunos.filter(aluno =>
+      aluno.nome.toLowerCase().includes(this.filter.toLowerCase()) ||
+      aluno.sobrenome.toLowerCase().includes(this.filter.toLowerCase())
+    );
+  }
+
+  onDelete(id: number): void {
     this.turmaAlunoService.deleteTurmaAluno(id).subscribe(() => {
-      this.loadTurmaAlunos();
+      this.displayDialog = false;
     });
   }
+
+
+  onEdit(id: number): void {
+    this.router.navigate(['/turma-aluno/edit', id]);
+  }
+
+  onView(id: number): void {
+    this.router.navigate(['/turma-aluno/view', id]);
+  }
+
+  newTurmaAluno(id: number): void{
+    this.router.navigate(['/turma-aluno/new', id]);
+  }
+
+  searchByAlunoId(alunoId: number): void {
+    if (alunoId !== null) {
+      this.turmaAlunoService.searchByAlunoId(alunoId).subscribe(
+        turmaAlunos => {
+          this.filteredTurmaAlunos = turmaAlunos;
+          this.errorMessage = '';
+        },
+        error => {
+          this.errorMessage = error.error.message;
+          this.filteredTurmaAlunos = [];
+        }
+      );
+    }
+  }
+
+  showTurmasDialog(aluno: Aluno): void {
+    this.selectedAluno = aluno;
+    this.turmaAlunoService.searchByAlunoId(aluno.id).subscribe(turmas => {
+      if(turmas.length > 0){
+        this.turmaAlunos = turmas;
+        this.displayDialog = true;
+      }
+    });
+  }
+
 }
